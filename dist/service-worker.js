e473e4dbd0cc9752f5a961c58431909e8619d190
Bakkar7 <63618629+Bakkar7@users.noreply.github.com>
2020-08-29 18:36:14 +0400

    Add files via upload

---
+++ dist/service-worker.js
@@ -0,0 +1,25 @@
+importScripts("/precache-manifest.14b1c398cbae646779e701de1ad136b7.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable no-undef,no-restricted-globals */
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.core.setCacheNameDetails({
+  prefix: "saleor-store-front"
+});
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+workbox.routing.registerRoute(
+  new RegExp("^http.*(?:png|gif|jpg|jpeg|webp|svg)"),
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/index.html"),
+  {
+    blacklist: [new RegExp("/graphql"), new RegExp("/dashboard")]
+  }
+);
+
